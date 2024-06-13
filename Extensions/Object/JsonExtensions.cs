0cb008686a7d7b98dcf50869f97051fa4925a28c
rojo1585 <jorge_mendoza1585@hotmail.com>
2024-06-13 14:16:00 -0600

    Add proyect and Jsnsn extencion method

---
+++ Extensions/Object/JsonExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Red.ToolKit.Extensions.Object;
+
+public static class JsonExtensions
+{
+    public static string ToJson(this object obj)
+    {
+        string jsonString;
+        using MemoryStream stream = new();
+
+        JsonSerializer.Serialize(stream, obj);
+        stream.Seek(0, SeekOrigin.Begin);
+
+        using (StreamReader sr = new(stream))
+        {
+            jsonString = sr.ReadToEnd();
+        }
+
+        return jsonString;
+    }
+}
