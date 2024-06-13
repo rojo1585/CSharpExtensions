e52886e433ff8da4dac12b9f887fc31f8aee20e7
rojo1585 <jorge_mendoza1585@hotmail.com>
2024-06-13 16:34:06 -0600

    add Strign JsonDeserialize

---
+++ Extensions/String/JsonExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Red.ToolKit.Extensions.String;
+
+public static class JsonExtensions
+{
+    public static T? DeserializeJson<T>(this string str)
+    {
+        try
+        {
+            T? obj;
+            using (MemoryStream stream = new(Encoding.UTF8.GetBytes(str)))
+            {
+                obj = JsonSerializer.Deserialize<T>(stream);
+            }
+            return obj;
+        }
+        catch (JsonException)
+        {
+            throw new JsonException("No fue posible convertir la cadena");
+        }
+    }
+}
