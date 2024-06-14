af378ac70f05ca1103fe1fb7b808684e4c6d0112
rojo1585 <jorge_mendoza1585@hotmail.com>
2024-06-14 12:35:52 -0600

    -add InitializingNullProperties Method for eliminates null problems

--- Extensions/Object/JsonExtensions.cs
+++ Extensions/Object/JsonExtensions.cs
@@ -1,10 +1,12 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
 
 namespace Red.ToolKit.Extensions.Object;
 
 public static class JsonExtensions
 {
-    public static string ToJson(this object obj)
+    private static readonly Dictionary<Type, object> defaultValues = [];
+    public static string ToJson<T>(this T obj) where T : class
     {
         string jsonString;
         using MemoryStream stream = new();
@@ -19,4 +21,63 @@ public static class JsonExtensions
 
         return jsonString;
     }
+
+    public static T InitializingNullProperties<T>(this T? obj) where T : class
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+
+        PropertyInfo[] properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (PropertyInfo property in properties)
+        {
+            if (property.GetIndexParameters().Length > 0 || !property.CanWrite)
+                continue;
+
+            object? value = property.GetValue(obj);
+            Type propertyType = property.PropertyType;
+
+            if (value == null || IsNullableAndDefault(value, propertyType))
+            {
+                object? defaultValue = GetDefaultValue(propertyType);
+                property.SetValue(obj, defaultValue);
+            }
+        }
+        return obj;
+    }
+
+    private static object? GetDefaultValue(Type type)
+    {
+        var underlyingType = Nullable.GetUnderlyingType(type) ?? type;
+
+        if (underlyingType == typeof(string))
+            return string.Empty;
+        else if (underlyingType.IsValueType)
+        {
+            if (defaultValues.TryGetValue(underlyingType, out var value))
+                return value;
+
+            value = Activator.CreateInstance(underlyingType);
+            if (value is not null)
+                defaultValues.Add(underlyingType, value);
+
+            return value;
+        }
+        else
+        {
+            try
+            {
+                var instance = Activator.CreateInstance(underlyingType);
+                InitializingNullProperties(instance);
+                return instance;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+    private static bool IsNullableAndDefault(object value, Type type) =>
+        value == null && Nullable.GetUnderlyingType(type) != null;
+
+
+
 }
